3cdd191ddf0e27efe9a0700edca4d0a1f5725973
ermias-vm <ermiasvalls.m@gmail.com>
2025-11-12 23:40:06 +0100

    2025-04-10: ex 1 and 2

--- lab_exams/L1/2025_04_10/A/ex2/user.c
+++ lab_exams/L1/2025_04_10/A/ex2/user.c
@@ -4,11 +4,14 @@ char buff[24];
 
 int pid;
 
-int __attribute__ ((__section__(".text.main")))
-  main(void)
-{
-    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+int __attribute__((__section__(".text.main"))) main(void) {
+    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so
+     * it will raise an exception */
+    /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-  while(1) { }
+    char *msg = "\nBAKA BAKA is back\n";
+    write(1, msg, strlen(msg));
+
+    while (1) {
+    }
 }
